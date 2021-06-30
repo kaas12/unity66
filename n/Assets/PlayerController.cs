34a3ca7a9fa47557105e659c5390229aea5ab5b5
kaas12 <noah.vanpotten@gmail.com>
2021-06-30 21:23:11 +0100

    a

---
+++ n/Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(5, 5);
+    public Transform ply;
+    public Transform ShootingPoint;
+    public GameObject Bullet;
+
+    float PosX = 0f;
+    float PosY = -4f;
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+
+        Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
+
+        PosX = Mathf.Clamp(PosX + speed.x * inputX, -2.8f, 2.8f);
+        PosY = Mathf.Clamp(PosY + speed.y * inputY, -4.5f, 0f);
+
+        Vector3 position = new Vector3(PosX, PosY, 0);
+        ply.transform.position = position;
+
+        if (Input.GetKeyDown("space"))
+        {
+            Shoot();
+        }
+
+    }
+    public void Shoot() {
+        Instantiate(Bullet, ShootingPoint.position, ShootingPoint.rotation);
+    }
+}
---
+++ n/Assets/Rock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    public GameObject Faller;
+    public float spawnWait = 2;
+
+    public void CreateRock() 
+    {
+        float PosX = Random.Range(-2.7f, 2.7f);
+        Vector3 Position = new Vector3(PosX, 6, 0);
+        Instantiate(Faller, Position, Quaternion.Euler(0f, Random.Range(0f, 360f), 0f));
+    }
+
+    void Start() 
+    {
+        InvokeRepeating("CreateRock", 2f, 2f);
+        Debug.Log("hi");
+    }
+
+
+    public void Explode() 
+    {
+        Destroy(gameObject);
+    }
+}
